3a3f99e494c1d904c79ee57c5293bec84da52dea
Denis Zamolodchikov <d.zamolodchikov@bi.zone>
2021-07-26 19:25:13 +0300

    Revert "disable importing cert"
    This reverts commit bf73d2c8e4d0f12ded51c0e06a974b0c4c01e4b0.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,22 @@
 set -e
 set -o pipefail
 
-# K8S_CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+K8S_CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
-# if [ -f "$K8S_CA_CERT" ]; then
-#     if command -v update-ca-certificates &> /dev/null
-#     then
-#         cp $K8S_CA_CERT /usr/local/share/ca-certificates/k8s-ca.crt
-#         update-ca-certificates
-#         echo "Kubernetes CA certificate was aded to the system trust store"
-#     fi
-#     if command -v keytool &> /dev/null
-#     then
-#         keytool -storepass changeit -noprompt -trustcacerts -importcert -cacerts \
-#         -alias cacertk8s -file $K8S_CA_CERT
-#         echo "Kubernetes CA certificate was aded to the JAVA trust store"
-#     fi
-# fi
+if [ -f "$K8S_CA_CERT" ]; then
+    if command -v update-ca-certificates &> /dev/null
+    then
+        cp $K8S_CA_CERT /usr/local/share/ca-certificates/k8s-ca.crt
+        update-ca-certificates
+        echo "Kubernetes CA certificate was aded to the system trust store"
+    fi
+    if command -v keytool &> /dev/null
+    then
+        keytool -storepass changeit -noprompt -trustcacerts -importcert -cacerts \
+        -alias cacertk8s -file $K8S_CA_CERT
+        echo "Kubernetes CA certificate was aded to the JAVA trust store"
+    fi
+fi
 
 echo "Executing \"${@}\"..."
 
